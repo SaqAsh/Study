6685e0d44a0a77954f90eca8c1737238493e22c2
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-01-08 13:16:33 -0500

    implemented changes in the terminator class defintion, the class would take a singular string as a parameter before, now the class takes a list as the user may enter a list of stirngs as apps they want to terminate, allowing for better user experience

--- STUDY.py
+++ STUDY.py
@@ -20,16 +20,18 @@ class UserInputAndIO:
                 print("_________________")
                 print("Would you like to use existing settings for your current study session")
                 print("Please note that creating new settings will wipe old settings, would you like to continue? [Y/N]")
+                charSelect = input()
+                charSelect = charSelect.lower()
+                # I want this to be the cross roads, if the user presses yes then there is a new bit of information created, if the user 
+                #does not press yes, then it reads shit from the file
                 
-                
-            
-            self.InputString = input("Enter a program that makes you procrasinate!")
-            print(f"Input from user has successfully been recorded {self.InputString}")
-            charInput = input("Would you like to add another app?! [Y/N]")
-            charInput = charInput.lower() #allows it so no matter what the userinputs, the reading of the string will be the same
-            self.InputCounter +=1
-            print(f"The number of apps that you have currently schedculed to terminate{self.InputCounter}")
-            self.GetUserInput() if charInput == "y" else None 
+                self.InputString = input("Enter a program that makes you procrasinate!")
+                print(f"Input from user has successfully been recorded {self.InputString}")
+                charInput = input("Would you like to add another app?! [Y/N]")
+                charInput = charInput.lower() #allows it so no matter what the userinputs, the reading of the string will be the same
+                self.InputCounter +=1
+                print(f"The number of apps that you have currently schedculed to terminate{self.InputCounter}")
+                self.GetUserInput() if charInput == "y" else None 
         except ValueError as e:
             print (f"Error in getting user input:{e}")
     def GetUserTimeInput(self):
@@ -46,8 +48,8 @@ class UserInputAndIO:
             print(f"Error in getting user input: {e}")
             
 class Terminator:
-    def __init__(self, App_Name):
-        self.App_Name = App_Name
+    def __init__(self, App_Name_List):
+        self.App_Name_List = App_Name_List
         self.ProgramCounter = -1
 
     def terminate(self):
@@ -55,17 +57,18 @@ class Terminator:
             raise ValueError("App Name cannot be None")
         cur_processes = psutil.process_iter(['pid', 'name'])
         for each_process in cur_processes:
-            if each_process.info.get('name') == self.App_Name:
-                try:
-                    # Check if the process is running before attempting to terminate
-                    if psutil.pid_exists(each_process.info['pid']):
-                        each_process.terminate()
-                        ProgramCounter+=1 # this is the counter that tracks how many times the user tries to log back onto something to procrasinate
-                        print(f"Terminated process with name: {self.App_Name}")
-                    else:
-                        print(f"Process with name {self.App_Name} not found.")
-                except psutil.NoSuchProcess as e:
-                    print(f"Error terminating process: {e}")
+            for each_item in self.App_Name_List:
+                if each_process.info.get('name') == each_item:
+                    try:
+                        # Check if the process is running before attempting to terminate
+                        if psutil.pid_exists(each_process.info['pid']):
+                            each_process.terminate()
+                            ProgramCounter+=1 # this is the counter that tracks how many times the user tries to log back onto something to procrasinate
+                            print(f"Terminated process with name: {self.App_Name}")
+                        else:
+                            print(f"Process with name {self.App_Name} not found.")
+                    except psutil.NoSuchProcess as e:
+                        print(f"Error terminating process: {e}")
 
 
 def main():
