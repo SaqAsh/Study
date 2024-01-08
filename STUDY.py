d82672ec6a6b4710c627dcc84f8fd8063ec80809
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-01-07 22:34:43 -0500

    Changed file name to help with running the file without using an escape sequence

---
+++ STUDY.py
@@ -0,0 +1,49 @@
+import psutil
+import time
+
+class UserInput:
+    def __init__(self, InputString):
+        self.InputString = InputString
+    def GetUserInput(self):
+        try:
+            print("yeofam")
+        except ValueError as e:
+            print (f"Error in getting userinput:{e}")
+        
+
+class Terminator:
+    def __init__(self, App_Name):
+        self.App_Name = App_Name
+
+    def terminate(self):
+        if self.App_Name is None:
+            raise ValueError("App Name cannot be None")
+        cur_processes = psutil.process_iter(['pid', 'name'])
+        for each_process in cur_processes:
+            if each_process.info.get('name') == self.App_Name:
+                try:
+                    # Check if the process is running before attempting to terminate
+                    if psutil.pid_exists(each_process.info['pid']):
+                        each_process.terminate()
+                        print(f"Terminated process with name: {self.App_Name}")
+                    else:
+                        print(f"Process with name {self.App_Name} not found.")
+                except psutil.NoSuchProcess as e:
+                    print(f"Error terminating process: {e}")
+
+
+def main():
+    # Create an instance of the Terminator class
+    user_instance = UserInput
+    terminator_instance = Terminator(user_instance.GetUserInput)
+    terminator_instance2 = Terminator("FaceTime")
+
+    # Call the terminate method
+    while True:
+        terminator_instance.terminate()
+        terminator_instance2.terminate()
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
