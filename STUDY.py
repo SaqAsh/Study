03427f5c2ec5c91cfd8465419d6a33dedbd81d53
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-01-08 10:59:20 -0500

    updated user input class

--- STUDY.py
+++ STUDY.py
@@ -1,16 +1,44 @@
 import psutil
 import time
 
+
+#this is the class for the timer, on how long the user will be 
+
+#class Timer
+
+
+
+
+# this is the class for the basic user input for what program they want termianted, expand on the user input class at a later date
+# add a function to start the timer, and how many hours you want to study for, this is the end time for this program, once this timer has eneded, that is where the 
 class UserInput:
-    def __init__(self, InputString):
-        self.InputString = InputString
+    def __init__(self):
+        self.InputString = None
+        self.InputTime = None
+        self.InputPassword = None
     def GetUserInput(self):
         try:
-            self.InputString = input()
+            self.InputString = input("Enter a program that makes you procrasinate!")
             print(f"Input from user has successfully been recorded {self.InputString}")
         except ValueError as e:
-            print (f"Error in getting userinput:{e}")
+            print (f"Error in getting user input:{e}")
+    def GetUserTimeInput(self):
+        try:
+            self.InputTime = float(input("Enter the amount of time you want to study for"))
+            print(f"Goal time successfully stored {self.InputTime}")
+        except ValueError as e:
+            print(f"Error in getting user input: {e}")
+    def GetUserPassword(self):
+        try:
+            self.InputPassword = int(input("Enter the password to exit out of study period"))
+            print(f"Password Successfully Stored: {self.InputPassword}")
+        except ValueError as e:
+            print(f"Error in getting user input: {e}")
         
+        
+        
+
+
 
 class Terminator:
     def __init__(self, App_Name):
@@ -36,7 +64,8 @@ class Terminator:
 def main():
     # Create an instance of the Terminator class
     user_instance = UserInput
-    terminator_instance = Terminator(user_instance.GetUserInput)
+    Input = user_instance.GetUserInput()
+    terminator_instance = Terminator(Input)
     terminator_instance2 = Terminator("FaceTime")
 
     # Call the terminate method
